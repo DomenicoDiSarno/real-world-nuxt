9c1d93ddef8a4d36a762dea9e81d4151c208636b
Domenico Di Sarno <domenico.disarno@abstract.it>
2021-01-23 16:43:26 +0100

    feat: add seo and dynamic routes

---
+++ pages/event/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create a new event #' + this.id,
+        },
+      ],
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    },
+  },
+}
+</script>
